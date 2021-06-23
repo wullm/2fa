771aeff381917368e1d217e9d389f8e552cdd1aa
Willem Elbers <willem.h.elbers@durham.ac.uk>
2021-06-23 10:56:05 +0100

    Update headers

--- src/input.c
+++ src/input.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * This file is part of Mitos.
+ * This file is part of 3FA.
  * Copyright (c) 2020 Willem Elbers (whe@willemelbers.com)
  *
  * This program is free software: you can redistribute it and/or modify
